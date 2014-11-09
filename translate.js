5bda7c897bbfc5d5f6865a23eff209ee18bd1935
Sébastien <Sbastien@Sonic>
2014-11-09 23:23:54 +0100

    Première version

--- translate.js
+++ translate.js
@@ -1,1 +1,43 @@
 var _ = require('lodash');
+
+var rosettaStone = {
+	'a' : '.-',	
+	'b' : '-...',	
+	'c' : '-.-.',	
+	'd' : '-..',	
+	'e' : '.',	
+	'f' : '..-.',	
+	'g' : '--.',	
+	'h' : '....',	
+	'i' : '..',	
+	'j' : '.---',	
+	'k' : '-.-',	
+	'l' : '.-..',	
+	'm' : '--',	
+	'n' : '-.',	
+	'o' : '---',	
+	'p' : '.--.',	
+	'q' : '--.-',	
+	'r' : '.-.',	
+	's' : '...',	
+	't' : '-',	
+	'u' : '..-',	
+	'v' : '...-',	
+	'w' : '.--',	
+	'x' : '-..-',	
+	'y' : '-.--',	
+	'z' : '--..'	
+}
+
+var string2translate = 'bonjour';
+
+var translation = '';
+
+_.forEach(
+	string2translate.split(''),
+	function(char) {
+		translation = translation.concat(rosettaStone[char]);
+	}
+);
+
+console.log(translation);
