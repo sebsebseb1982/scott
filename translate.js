042cc3111c15b383b47b9af66d6dde0ad67e247e
Sébastien <Sbastien@Sonic>
2014-11-09 23:58:31 +0100

    Test

--- translate.js
+++ translate.js
@@ -1,5 +1,9 @@
 var _ = require('lodash');
 
+var tiTime = 0.2;
+
+var gpioNumber = 2;
+
 var rosettaStone = {
 	'a' : '.-',	
 	'b' : '-...',	
@@ -26,7 +30,8 @@ var rosettaStone = {
 	'w' : '.--',	
 	'x' : '-..-',	
 	'y' : '-.--',	
-	'z' : '--..'	
+	'z' : '--..',
+	' ' : '/'
 }
 
 var string2translate = 'bonjour';
@@ -36,8 +41,45 @@ var translation = '';
 _.forEach(
 	string2translate.split(''),
 	function(char) {
-		translation = translation.concat([rosettaStone[char], ' ']);
+		translation = translation.concat(rosettaStone[char], ' ');
 	}
 );
 
 console.log(translation);
+
+var blink = function(duration) {
+	return 'gpio write ' + gpioNumber + ' 1; sleep ' + duration + '; gpio write ' + gpioNumber + ' 0; sleep ' + tiTime + ';'
+}
+
+var wait = function(duration) {
+	return 'sleep ' + duration + ';'
+}
+
+var command = '';
+
+_.forEach(
+	translation.split(''),
+	function(char) {
+		switch (char) {
+		    case '.':
+		        command = command.concat(blink(tiTime));
+		        break;
+		    case '-':
+		    	command = command.concat(blink(tiTime * 3));
+		        break;
+		    case ' ':
+		    	command = command.concat(wait(tiTime * 2));
+		    	break;
+		    case '/':
+		    	command = command.concat(wait(tiTime * 6));
+		        break;
+		} 
+	}
+);
+
+console.log(command);
+
+var exec = require('child_process').exec;
+exec(command);
+
+
